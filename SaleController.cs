0d3aa12eef1c55ffcf14ed30e2859192d8e49a44
SoftwareInformation <tensionfree848@gmail.com>
2017-03-30 05:35:29 -0700

    Add files via upload

---
+++ SaleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventorySystem.Controllers
+{
+    public class SaleController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Sales";
+
+            return View();
+        }
+        public ActionResult Info() {
+
+            return View();
+        }
+
+    }
+}
